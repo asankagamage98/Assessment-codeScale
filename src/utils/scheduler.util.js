7cd2538090a30dfc96486b2452de92d527ef64e7
asankagamage98 <asankagamage1998@gmail.com>
2024-01-12 12:40:06 +0530

    setup vercel.json file

--- src/utils/scheduler.util.js
+++ src/utils/scheduler.util.js
@@ -1,11 +1,10 @@
 const cron = require("node-cron");
 const { sendWeatherReportToAllUsers } = require("../services/user.service");
 
-// Use the scheduleTime variable here
+//  declare scheduleTimes
 const scheduleTime = "*/1 * * * *"; // 1min
 // const scheduleTime = "* */3 * * *"; // 3 hours
 
-// Export a function to start the scheduler
 module.exports = () => {
   cron.schedule(scheduleTime, () => {
     console.log("Running email sending job...");
