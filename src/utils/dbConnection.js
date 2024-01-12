d7f5d13152b5d15183018ce97404a0142a45035a
asankagamage98 <asankagamage1998@gmail.com>
2024-01-12 23:16:40 +0530

    changed dbconnection code

--- src/utils/dbConnection.js
+++ src/utils/dbConnection.js
@@ -1,37 +1,13 @@
-// const { default: mongoose } = require("mongoose");
+const mongoose = require('mongoose')
 
-// const url = process.env.MONGODB_URL;
-// const dbConnect = () => {
-//   mongoose.connect(url);
-//   console.info("db connection is working successfully");
-// };
-
-// module.exports = {
-//   dbConnect,
-// };
-
-
-
-const { default: mongoose } = require("mongoose");
-
-const url = process.env.MONGODB_URL;
-
-const dbConnect = () => {
-  mongoose.connect(url, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  });
-
-  const db = mongoose.connection;
-
-  db.on("error", (error) => {
-    console.error("MongoDB connection error:", error);
-  });
-
-  db.once("open", () => {
-    console.info("MongoDB connection is successful");
-  });
-};
+const dbConnect = async () => {
+    try {
+        await mongoose.connect(process.env.MONGODB_URL)
+        console.log("Connect to MongoDB successfully")
+    } catch (error) {
+        console.log("Connect failed " + error.message )
+    }
+}
 
 module.exports = {
   dbConnect,
