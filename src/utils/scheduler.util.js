1cea7588d05a2345d431c090484c7b9039160ade
asankagamage98 <asankagamage1998@gmail.com>
2024-01-12 12:11:44 +0530

    changed  hours of send wheather report

--- src/utils/scheduler.util.js
+++ src/utils/scheduler.util.js
@@ -2,7 +2,8 @@ const cron = require("node-cron");
 const { sendWeatherReportToAllUsers } = require("../services/user.service");
 
 // Use the scheduleTime variable here
-const scheduleTime = "* */3 * * *"; // 3 hrs
+const scheduleTime = "*/1 * * * *"; // 1min
+// const scheduleTime = "* */3 * * *"; // 3 hours
 
 // Export a function to start the scheduler
 module.exports = () => {
